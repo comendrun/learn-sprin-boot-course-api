c2af23d38b35bfcc22ea090839c5a263ff44ec84
comendrun <kamran.ruhani@gmail.com>
2022-10-21 12:04:50 +0200

    Initial Commit

---
+++ src/main/java/com/rms/learnspringboot/learnsprinbootcourseapi/LearnSprinBootCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.rms.learnspringboot.learnsprinbootcourseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSprinBootCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSprinBootCourseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rms/learnspringboot/learnsprinbootcourseapi/LearnSprinBootCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rms.learnspringboot.learnsprinbootcourseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSprinBootCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
